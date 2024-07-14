ab77b7a80ae5b4c14adc7f3f2d1818a4a2e6bcbc
omarmosharawi <ocr115081@gmail.com>
2024-07-14 10:19:22 +0300

    set up checkout page

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,2 +1,7 @@
+from django.urls import path
+from checkout import views
+
+
 urlpatterns = [
+    path('/order', views.makeOrder, name='checkout.order')
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserInfoForm
+from store.models import Product, Cart, Order
 
 # Create your views here.
+
+
+def makeOrder(request):
+    if request.method != 'POST':
+        return redirect('store.checkout')
+
+    form = UserInfoForm(request.POST)
+    if form.is_valid():
+        cart = Cart.objects.filter(session=request.session.session_key).last()
+        products = Product.objects.filter(pk__in=cart.item)
+
+        total = 0
+        for item in products:
+            total += item.price
+
+        if total <= 0:
+            return redirect('store.cart')
+
+        order = Order.objects.create(customer=form.cleaned_data, total=total)
+        for product in products:
+            order.orderproduct_set.create(product_id=product.id, price=product.price)
+
+        cart.delete()
+        return redirect('store.checkout-complete')
+
+    else:
+        return redirect('store.checkout')
